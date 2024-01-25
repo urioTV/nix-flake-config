740136ff41c00b19622ee81225e1c39ede0743f5
urioTV <uriootv@protonmail.com>
2024-01-25 18:46:01 +0100

    grub

--- configuration.nix
+++ configuration.nix
@@ -28,9 +28,20 @@
 
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.systemd-boot.enable = true;
 
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      # efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+    };
+    grub = {
+      enable = true;
+      efiSupport = true;
+      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+      device = "nodev";
+    };
+  };
 
   zramSwap.enable = true;
 

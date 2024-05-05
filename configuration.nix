f8e5883307e55b7e798325d28b5258604c29ca1d
urioTV <uriootv@protonmail.com>
2024-05-05 17:02:54 +0200

    updated flake.lock, disabled chaotic mesa, added hyprland cachix

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,12 @@
     ./host-hyprland
   ];
 
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys =
+      [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
+
   # Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 

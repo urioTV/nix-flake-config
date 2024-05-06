bdd66ffbf3463c49d5b9951cd44b9a1fe6dbca90
urioTV <uriootv@protonmail.com>
2024-05-06 13:30:50 +0200

    wallpapers

--- stylixHome.nix
+++ stylixHome.nix
@@ -1,11 +1,11 @@
 { config, lib, pkgs, ... }: {
   stylix.autoEnable = true;
-  stylix.image = ./tech-driad.png;
+  stylix.image = ./space-guy.png;
   stylix.base16Scheme = ./catppuccin/mocha.yaml;
   stylix.targets.gnome.enable = true;
   stylix.polarity = "dark";
   stylix.targets.vscode.enable = false;
-  stylix.cursor.package = pkgs.catppuccin-cursors.mochaDark;
-  stylix.cursor.name = "Catppuccin-Mocha-Dark-Cursors";
+  # stylix.cursor.package = pkgs.catppuccin-cursors.mochaDark;
+  # stylix.cursor.name = "Catppuccin-Mocha-Dark-Cursors";
   # stylix.cursor.size = 24;
 }

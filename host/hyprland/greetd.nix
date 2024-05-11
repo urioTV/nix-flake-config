392293c70aaf73265fb6d84c828ebac19e4a340c
urioTV <uriootv@protonmail.com>
2024-05-12 01:41:16 +0200

    fixed screenshot, disabled tearing and swaync panel

--- host/hyprland/greetd.nix
+++ host/hyprland/greetd.nix
@@ -11,7 +11,7 @@ in {
     settings = {
       default_session = {
         command = "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session}";
-        user = "urio";
+        user = "greeter";
       };
     };
   };

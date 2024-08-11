bb1fbc58d9349a02f3b1249357c74fdc9537d1a8
urioTV <uriootv@protonmail.com>
2024-08-11 15:00:38 +0200

    nix-settings.nix file

--- configuration.nix
+++ configuration.nix
@@ -5,26 +5,6 @@
     ./host
   ];
 
-  # Flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  nix.settings = {
-    substituters = [
-      "https://hyprland.cachix.org"
-      "https://nix-gaming.cachix.org"
-      "https://nix-citizen.cachix.org"
-      "https://nix-community.cachix.org"
-    ];
-    trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
-  nix.settings.trusted-users = [ "root" "urio" ];
-
   networking.hostName = "konrad-m18"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 

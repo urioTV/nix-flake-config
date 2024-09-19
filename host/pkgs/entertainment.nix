a18c58e221c65009cd8414b7d0095b91afb3b15f
urioTV <uriootv@protonmail.com>
2024-09-19 17:56:33 +0200

    bump

--- host/pkgs/entertainment.nix
+++ host/pkgs/entertainment.nix
@@ -15,11 +15,11 @@
     steamtinkerlaunch
     # inputs.nix-citizen.packages.${system}.star-citizen
     # star-citizen
-    (inputs.nix-citizen.packages.${system}.star-citizen.override (prev: {
-          # Recommended to keep the previous overrides
-          preCommands = "export radv_zero_vram=true";
-          useUmu = true;
-        }))
+    # (inputs.nix-citizen.packages.${system}.star-citizen.override (prev: {
+    #       # Recommended to keep the previous overrides
+    #       preCommands = "export radv_zero_vram=true";
+    #       useUmu = true;
+    #     }))
   ];
 
 }

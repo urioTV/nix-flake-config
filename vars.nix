9fcf8869bd1153e9e198f5dc33990e779c6c5dc1
urioTV <uriootv@protonmail.com>
2024-12-28 17:43:03 +0100

    more cleaning up

--- vars.nix
+++ vars.nix
@@ -4,12 +4,12 @@ with lib;
   options.vars = {
     wallpaper = mkOption {
       type = types.str;
-      default = "${./kosciejo.png}";
+      default = "${./media/kosciejo.png}";
       description = "My wallpaper path";
     };
     base16Scheme = mkOption {
       type = types.str;
-      default = "${./base16Schemes/mocha.yaml}";
+      default = "${./media/base16Schemes/mocha.yaml}";
       description = "My base16 scheme path";
     };
   };

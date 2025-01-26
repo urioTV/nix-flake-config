bb7cb5e44fd5be8337356b5fce5d5261bd4c4acb
urioTV <uriootv@protonmail.com>
2025-01-27 00:32:29 +0100

    nushell

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -7,72 +7,18 @@
   ...
 }:
 {
+  imports = [
+    ./shells.nix
+  ];
+
   programs = {
     git = {
       enable = true;
       userEmail = "uriootv@protonmail.com";
       userName = "urioTV";
     };
-    nushell = {
-      enable = true;
-      extraConfig = ''$env.config.show_banner = false'';
-    };
-    zsh = {
-      enable = true;
-      antidote = {
-        enable = false;
-        plugins = [
-          "zsh-users/zsh-autosuggestions"
-          "zsh-users/zsh-completions"
-        ];
-      };
-      prezto = {
-        enable = true;
-        pmodules = [
-          "environment"
-          "terminal"
-          "editor"
-          "history"
-          "directory"
-          "spectrum"
-          "utility"
-          "completion"
-          "prompt"
-          "autosuggestions"
-          "git"
-        ];
-      };
-      shellAliases = {
-        nix = "noglob nix";
-        nixos-rebuild = "noglob nixos-rebuild";
-      };
-
-    };
-    carapace = {
-      enable = true;
-      enableZshIntegration = true;
-      enableNushellIntegration = true;
-    };
     bat = {
       enable = true;
-    };
-    starship = {
-      enable = true;
-      enableZshIntegration = true;
-      enableNushellIntegration = true;
-      settings = {
-        nix_shell = {
-          disabled = false;
-          impure_msg = "";
-          symbol = "";
-          format = "[$symbol$state]($style) ";
-        };
-        shlvl = {
-          disabled = false;
-          symbol = "λ ";
-        };
-        haskell.symbol = " ";
-      };
     };
     obs-studio = {
       enable = true;

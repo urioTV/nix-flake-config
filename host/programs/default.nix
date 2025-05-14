261b7c0c4d775f9d5945a69e774d00707a6ddf34
urioTV <uriootv@protonmail.com>
2025-05-14 20:17:56 +0200

    updated kernel

--- host/programs/default.nix
+++ host/programs/default.nix
@@ -31,6 +31,17 @@
     adb = {
       enable = true;
     };
+    direnv = {
+      enable = true;
+      package = pkgs.direnv;
+      silent = false;
+      loadInNixShell = true;
+      direnvrcExtra = "";
+      nix-direnv = {
+        enable = true;
+        package = pkgs.nix-direnv;
+      };
+    };
   };
 
 }

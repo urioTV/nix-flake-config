96af3a42dee2b268c18002b108203a34f8b277d0
urioTV <uriootv@protonmail.com>
2025-05-23 17:12:22 +0200

    NTSYNC patch for 6.12 kernel

--- host/boot-kernel-stuff/default.nix
+++ host/boot-kernel-stuff/default.nix
@@ -6,14 +6,37 @@
   ...
 }:
 {
+  # boot.kernelPackages = pkgs.linuxPackages_zen;
+  # boot.kernelPackages = pkgs.linuxPackages_cachyos-lto;
+  # boot.kernelPackages = pkgs.linuxPackages_cachyos;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
+
+  # NTSYNC patch
+  boot.kernelPatches = [
+    {
+      name = "ntsync";
+      patch = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/CachyOS/kernel-patches/master/6.12/0005-ntsync.patch";
+        sha256 = "sha256-KaSOBV81NYET0kXMNwuFvIuy1pf3R04lNuE3ZjA4oak=";
+      };
+      extraConfig = ''
+        NTSYNC m
+      '';
+    }
+  ];
 
   boot.initrd.kernelModules = [ "ntsync" ];
 
-  # boot.kernelPackages = pkgs.linuxPackages_zen;
-  # boot.kernelPackages = pkgs.linuxPackages_cachyos-lto;
-  boot.kernelPackages = pkgs.linuxPackages_cachyos;
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
-  # boot.kernelPackages = pkgs.linuxPackages_6_12;
+  users.groups.ntsync = { };
+
+  services.udev.extraRules = ''
+    KERNEL=="ntsync", GROUP="ntsync", MODE="0664"
+  '';
+
+  users.users.urio = {
+    extraGroups = [ "ntsync" ];
+  };
 
   boot.kernelParams = [ "amdgpu.dcdebugmask=0x10" ];
 

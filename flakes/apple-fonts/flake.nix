f77c7d0849c89341ad003ffa77ed208220729dbe
urioTV <uriootv@protonmail.com>
2025-07-08 14:44:04 +0200

    Update apple-fonts flake dependencies and add flake-utils
    Refactor flake.nix to use flake-utils.lib.eachDefaultSystem for cleaner
    system handling. Add flake-utils input to apple-fonts lockfile and
    adjust dependency tree structure.

--- flakes/apple-fonts/flake.nix
+++ flakes/apple-fonts/flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     sf-pro = {
       url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
       flake = false;
@@ -36,90 +37,91 @@
   };
 
   outputs =
-    inputs@{ self, ... }:
-    let
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-      forEachSystem = inputs.nixpkgs.lib.genAttrs systems;
-    in
     {
-      packages = forEachSystem (
-        system:
-        let
-          pkgs = inputs.nixpkgs.legacyPackages.${system};
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-          unpackPhase = pkgName: ''
-            runHook preUnpack
-            undmg $src
-            7z x '${pkgName}'
-            7z x 'Payload~'
-            runHook postUnpack
-          '';
+        unpackPhase = pkgName: ''
+          runHook preUnpack
+          undmg $src
+          7z x '${pkgName}'
+          7z x 'Payload~'
+          runHook postUnpack
+        '';
 
-          commonInstall = ''
-            mkdir -p "$out/share/fonts"
-            mkdir -p "$out/share/fonts/opentype"
-            mkdir -p "$out/share/fonts/truetype"
-          '';
+        commonInstall = ''
+          mkdir -p "$out/share/fonts"
+          mkdir -p "$out/share/fonts/opentype"
+          mkdir -p "$out/share/fonts/truetype"
+        '';
 
-          commonBuildInputs = builtins.attrValues { inherit (pkgs) undmg p7zip; };
+        commonBuildInputs = with pkgs; [
+          undmg
+          p7zip
+        ];
 
-          makeAppleFont = (
-            name: pkgName: src:
-            pkgs.stdenvNoCC.mkDerivation {
-              inherit name src;
+        makeAppleFont =
+          name: pkgName: src:
+          pkgs.stdenvNoCC.mkDerivation {
+            inherit name src;
 
-              unpackPhase = unpackPhase pkgName;
+            unpackPhase = unpackPhase pkgName;
+            buildInputs = commonBuildInputs;
+            setSourceRoot = "sourceRoot=`pwd`";
 
-              buildInputs = commonBuildInputs;
-              setSourceRoot = "sourceRoot=`pwd`";
-
-              installPhase =
-                ''runHook preInstall''
-                + commonInstall
-                + ''
-                  find -name \*.otf -exec mv {} "$out/share/fonts/opentype/" \;
-                  find -name \*.ttf -exec mv {} "$out/share/fonts/truetype/" \;
-                ''
-                + ''runHook preInstall'';
-            }
-          );
-
-          makeNerdAppleFont = (
-            name: pkgName: src:
-            pkgs.stdenvNoCC.mkDerivation {
-              inherit name src;
-
-              unpackPhase = unpackPhase pkgName;
-
-              buildInputs =
-                commonBuildInputs
-                ++ builtins.attrValues { inherit (pkgs) parallel nerd-font-patcher; };
-
-              setSourceRoot = "sourceRoot=`pwd`";
-
-              buildPhase = ''
-                runHook preBuild
-                find -name \*.ttf -o -name \*.otf -print0 | parallel --will-cite -j $NIX_BUILD_CORES -0 nerd-font-patcher --no-progressbars -c {}
-                runHook postBuild
+            installPhase =
+              ''
+                runHook preInstall
+              ''
+              + commonInstall
+              + ''
+                find -name \*.otf -exec mv {} "$out/share/fonts/opentype/" \;
+                find -name \*.ttf -exec mv {} "$out/share/fonts/truetype/" \;
+                runHook postInstall
               '';
+          };
 
-              installPhase =
-                ''runHook preInstall''
-                + commonInstall
-                + ''
-                  find -name \*.otf -maxdepth 1 -exec mv {} "$out/share/fonts/opentype/" \;
-                  find -name \*.ttf -maxdepth 1 -exec mv {} "$out/share/fonts/truetype/" \;
-                ''
-                + ''runHook preInstall'';
-            }
-          );
-        in
-        {
+        makeNerdAppleFont =
+          name: pkgName: src:
+          pkgs.stdenvNoCC.mkDerivation {
+            inherit name src;
+
+            unpackPhase = unpackPhase pkgName;
+            buildInputs =
+              commonBuildInputs
+              ++ (with pkgs; [
+                parallel
+                nerd-font-patcher
+              ]);
+            setSourceRoot = "sourceRoot=`pwd`";
+
+            buildPhase = ''
+              runHook preBuild
+              find -name \*.ttf -o -name \*.otf -print0 | parallel --will-cite -j $NIX_BUILD_CORES -0 nerd-font-patcher --no-progressbars -c {}
+              runHook postBuild
+            '';
+
+            installPhase =
+              ''
+                runHook preInstall
+              ''
+              + commonInstall
+              + ''
+                find -name \*.otf -maxdepth 1 -exec mv {} "$out/share/fonts/opentype/" \;
+                find -name \*.ttf -maxdepth 1 -exec mv {} "$out/share/fonts/truetype/" \;
+                runHook postInstall
+              '';
+          };
+      in
+      {
+        packages = {
           sf-pro = makeAppleFont "sf-pro" "SF Pro Fonts.pkg" inputs.sf-pro;
           sf-pro-nerd = makeNerdAppleFont "sf-pro-nerd" "SF Pro Fonts.pkg" inputs.sf-pro;
 
@@ -143,7 +145,7 @@
 
           ny = makeAppleFont "ny" "NY Fonts.pkg" inputs.ny;
           ny-nerd = makeNerdAppleFont "ny-nerd" "NY Fonts.pkg" inputs.ny;
-        }
-      );
-    };
+        };
+      }
+    );
 }

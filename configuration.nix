a3bfd4a5f027e92980cdf94b171f166c83f1d9cd
urioTV <uriootv@protonmail.com>
2025-09-16 22:41:08 +0200

    ADD udev rules for DarkProject devices

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,20 @@
     ./host
   ];
 
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "darkproject-udev-rules";
+      destination = "/lib/udev/rules.d/70-darkproject.rules";
+      text = ''
+        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="342d", ATTRS{idProduct}=="e40f", TAG+="uaccess"
+        SUBSYSTEM=="usb",    ATTRS{idVendor}=="342d", ATTRS{idProduct}=="e40f", TAG+="uaccess"
+        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="2442", ATTRS{idProduct}=="b071", TAG+="uaccess"
+        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="342d", ATTRS{idProduct}=="e410", TAG+="uaccess"
+        SUBSYSTEM=="usb",    ATTRS{idVendor}=="342d", ATTRS{idProduct}=="e410", TAG+="uaccess"
+      '';
+    })
+  ];
+
   # services.journald = {
   #   storage = "persistent";
   #   extraConfig = ''

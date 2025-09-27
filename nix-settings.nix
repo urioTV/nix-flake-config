3eb1ab52f1f9441adb8e4a89a667bcd00719e44e
urioTV <uriootv@protonmail.com>
2025-09-27 14:07:17 +0200

    Enable Lix package set overlays and set nix.package
    Uncomment the nixpkgs.overlays configuration to use Lix package sets and
    explicitly set nix.package to the stable Lix package.

--- nix-settings.nix
+++ nix-settings.nix
@@ -7,19 +7,19 @@
 }:
 
 {
-  # nixpkgs.overlays = [
-  #   (final: prev: {
-  #     inherit (final.lixPackageSets.stable)
-  #       nixpkgs-review
-  #       nix-direnv
-  #       nix-eval-jobs
-  #       nix-fast-build
-  #       colmena
-  #       ;
-  #   })
-  # ];
+  nixpkgs.overlays = [
+    (final: prev: {
+      inherit (final.lixPackageSets.stable)
+        nixpkgs-review
+        # nix-direnv
+        nix-eval-jobs
+        nix-fast-build
+        colmena
+        ;
+    })
+  ];
 
-  # nix.package = pkgs.lixPackageSets.stable.lix;
+  nix.package = pkgs.lixPackageSets.stable.lix;
 
   nix.settings = {
     experimental-features = [

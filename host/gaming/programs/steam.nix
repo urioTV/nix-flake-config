69f569bc6fd6498d04a8c69850421051e13e7303
Konrad Lemański <45111726+urioTV@users.noreply.github.com>
2025-10-02 16:13:06 +0200

    Create Steam configuration module

---
+++ host/gaming/programs/steam.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  programs.steam = {
+    enable = true;
+    # Gamescope Fix
+    package = pkgs.steam.override {
+      # extraEnv = { SDL_VIDEODRIVER = "x11"; };
+      extraPkgs =
+        pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+          curl
+          openssl
+          xdg-utils
+        ];
+    };
+    extraCompatPackages = with pkgs; [
+      proton-ge-custom
+    ];
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+}
